473dfd0f15ff0db310f1d11d9f9bcbe2001a90ee
Tory Wheelwright <twheelwright@gmail.com>
2018-05-21 13:27:31 -0400

    new

--- init.el
+++ init.el
@@ -1,37 +1,111 @@
 ;;; package --- Summary: tory's emacs config
 ;;; Commentary:
 ;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; use `package.el` to install `use-package`, which will then be used to ensure
+;; that everything else is installed.
+
 (require 'package)
-(setq package-archives
-	'(("gnu" . "https://elpa.gnu.org/packages/")
-	  ("melpa-stable" . "https://stable.melpa.org/packages/")
-	  ("marmalade" . "https://marmalade-repo.org/packages/")
-	  ("melpa" . "https://melpa.org/packages/")))
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa"
+				 . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade"
+				 . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu"
+				 . "http://elpa.gnu.org/packages/"))
 (package-initialize)
-(package-refresh-contents)
-(defvar package-packages '(bracketed-paste
-			   dockerfile-mode
-			   editorconfig
-			   flycheck
-			   helm
-			   jedi
-			   json-mode
-			   monokai-theme
-			   rainbow-delimiters
-			   rust-mode
-			   undo-tree
-			   yaml-mode))
-(unless package-archive-contents
-  (package-refresh-contents))
-(dolist (package package-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+;;(require 'diminish)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; package configurations
+
+(use-package auto-package-update ; auto updates packages
+  :ensure t
+  :config (setq auto-package-update-delete-old-versions t)
+          (setq auto-package-update-hide-results t)
+          (auto-package-update-maybe))
+
+(use-package bracketed-paste
+  :ensure t
+  :config (bracketed-paste-enable))
+
+(use-package company
+  :ensure t)
+
+(use-package cython-mode
+  :ensure t)
+
+(use-package dockerfile-mode
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config (editorconfig-mode 1))
+
+(use-package elixir-mode
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :config (setq flycheck-clang-language-standard "c++14")
+          (setq flycheck-python-pylint-executable "python3")
+          (setq flycheck-python-flake8-executable "python3")
+          (setq flycheck-checker 'python-flake8)
+          (flycheck-add-next-checker 'python-flake8 'python-pylint)
+          (global-flycheck-mode))
+
+(use-package helm
+  :ensure t
+  :config (helm-mode 1)
+          (helm-autoresize-mode 1)
+          (setq helm-split-window-inside-p 1)
+  :bind   (("M-x" . helm-M-x)
+	   ("M-y" . helm-show-kill-ring)
+	   ("C-x b" . helm-mini)
+	   ("C-x C-f" . helm-find-files)
+	   ("C-x f" . helm-find-files)))
+
+(use-package jedi
+  :ensure t
+  :hook   (python-mode . 'jedi:setup)
+  :config (setq jedi:complete-on-dot t))
+
+(use-package json-mode
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook   (prog-mode . rainbow-delimiters-mode))
+
+(use-package rust-mode
+  :ensure t)
+
+(use-package undo-tree
+  :ensure t
+  :config (global-undo-tree-mode))
+
+(use-package yaml-mode
+  :ensure t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; low tech manual configuration
+
+;; theme
+(add-to-list 'load-path "~/.emacs.d")
+(load-theme 'indoors t)
 
 ;; ui stuff
 (setq inhibit-startup-screen t)
 (line-number-mode 0)
 (column-number-mode 0)
-(bracketed-paste-enable)
 (setq mode-line-format
       (list '(vc-mode vc-mode)
 	    ":"
@@ -40,8 +114,6 @@
 	    mode-line-position
 	    " "
 	    mode-line-modes))
-(load-theme 'monokai t)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 (setq linum-format "%3d| ")
 (global-linum-mode t)
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -54,19 +126,25 @@
       (switch-to-buffer-other-frame server-buf))))
 (add-hook 'server-done-hook 'delete-frame)
 
-;; helm
-(helm-mode 1)
-(helm-autoresize-mode 1)
-(setq helm-split-window-in-side-p 1)
-(setq helm-buffers-fuzzy-matching 1)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x f") 'helm-find-files)
+;; set max line lenghts for various languages
+(add-hook 'after-change-major-mode-hook
+	  '(lambda () (when (eq major-mode 'rust-mode)
+			(setq whitespace-line-column 99))))
+(add-hook 'after-change-major-mode-hook
+	  '(lambda () (when (eq major-mode 'c-mode)
+			(setq whitespace-line-column 80))))
+(add-hook 'after-change-major-mode-hook
+	  '(lambda () (when (eq major-mode 'c++-mode)
+			(setq whitespace-line-column 80))))
+(add-hook 'after-change-major-mode-hook
+	  '(lambda () (when (eq major-mode 'python-mode)
+			(setq whitespace-line-column 79))))
+(add-hook 'after-change-major-mode-hook
+	  '(lambda () (when (eq major-mode 'java-mode)
+			(setq whitespace-line-column 100))))
 
 ;; editor stuff
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(editorconfig-mode 1)
 (defun smarter-move-beginning-of-line (arg)
   (interactive "^p")
   (setq arg (or arg 1))
@@ -131,29 +209,11 @@
 (windmove-default-keybindings)
 (setq whitespace-style '(face lines-tail))
 (global-whitespace-mode)
-(add-hook 'after-change-major-mode-hook
-	  '(lambda () (when (eq major-mode 'rust-mode)
-			(setq whitespace-line-column 99))))
-(add-hook 'after-change-major-mode-hook
-	  '(lambda () (when (eq major-mode 'c-mode)
-			(setq whitespace-line-column 80))))
-(add-hook 'after-change-major-mode-hook
-	  '(lambda () (when (eq major-mode 'c++-mode)
-			(setq whitespace-line-column 80))))
-(add-hook 'after-change-major-mode-hook
-	  '(lambda () (when (eq major-mode 'python-mode)
-			(setq whitespace-line-column 79))))
-(add-hook 'after-change-major-mode-hook
-	  '(lambda () (when (eq major-mode 'java-mode)
-			(setq whitespace-line-column 100))))
-(editorconfig-mode 1)
 (delete-selection-mode)
 (global-undo-tree-mode)
 (add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace 1)))
 
 ;; c/++ stuff
-(add-hook 'c++-mode-hook
-    (lambda () (setq flycheck-clang-language-standard "c++14")))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (c-set-offset 'innamespace 0) ;; Don't indent namespaces
 (defadvice c-lineup-arglist (around my activate)
@@ -170,14 +230,27 @@ http://stackoverflow.com/a/23553882"
       0                             ; no additional indent
       ad-do-it)))                   ; default behavior)
 
-;; python stuff
-(setq flycheck-python-pylint-executable "python3")
-(setq flycheck-python-flake8-executable "python3")
-(add-hook 'python-mode-hook (lambda ()
-			      (setq flycheck-checker 'python-flake8)
-			      (flycheck-add-next-checker 'python-flake8 'python-pylint)))
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+;; company mode
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; this should probably be cleaned up to not use mode hooks to manupilate company-backends
+
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0)
+;;  (company-quickhelp-mode 1)
+(add-hook 'darwin-system-type-hook
+  (lambda ()
+    (setq company-c-headers-path-system
+      '("/usr/local/Cellar/gcc/6.2.0/include/c++/6.2.0"
+         "/usr/local/Cellar/gcc/6.2.0/lib/gcc/6/gcc/x86_64-apple-darwin15.6.0/6.2.0/include"
+         "/usr/local/Cellar/gcc/6.2.0/lib/gcc/6/gcc/x86_64-apple-darwin15.6.0/6.2.0/include-fixed"))))
+(add-hook 'gnu/linux-system-type-hook
+  (lambda ()
+    (setq company-c-headers-path-system
+      '("/usr/include/"
+         "/usr/lib/gcc/x86_64-linux-gnu/4.9.3/include"
+         "/usr/include/c++/4.9.3"))))
+(setq company-tooltip-align-annotations t)
 
 ;;; init.el ends here
 (custom-set-variables
@@ -187,7 +260,7 @@ http://stackoverflow.com/a/23553882"
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode undo-tree rust-mode rainbow-delimiters monokai-theme json-mode elpy editorconfig dockerfile-mode bracketed-paste))))
+    (elixir-mode auto-package-update yaml-mode use-package undo-tree rust-mode rainbow-delimiters monokai-theme leuven-theme json-mode jedi helm flycheck elpy editorconfig dockerfile-mode bracketed-paste))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
