d8ff5688d66b0814c834fc1fdf756239e11dd3c3
Tory Wheelwright <twheelwright@gmail.com>
2019-01-18 18:47:32 -0500

    add script to install git aliases

---
+++ git-aliases.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+git config --global alias.a add
+git config --global alias.s status
+git config --global alias.b branch
+git config --global alias.c checkout
+git config --global alias.o commit
+git config --global alias.l log
+git config --global alias.i init
+git config --global alias.n clone
+git config --global alias.d diff
+git config --global alias.m merge
+git config --global alias.p push
